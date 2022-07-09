e86eca088a37ac78e982522dca0eaf80bc9a137b
bbooss97 <bbooss97@gmail.com>
2022-07-09 22:02:47 +0200

    changes i have to change the best feautres and the controller

--- agent.py
+++ agent.py
@@ -9,7 +9,7 @@ class SelfAttention(torch.nn.Module):
         self.qDimension = qDimension
         self.kDimension = kDimension
         self.q = torch.nn.Linear(inputDimension, qDimension)
-        self.k = torch.nn.Linear(qDimension, kDimension)
+        self.k = torch.nn.Linear(inputDimension, kDimension)
         self.inputDimension = inputDimension
     def forward(self, input):
         q=self.q(input)
@@ -31,15 +31,26 @@ class AgentNetwork(torch.nn.Module):
     inputDimension=0
     qDimension=0
     kDimension=0
-    patches=0
+    nOfPatches=0
     stride=0
+    patchesDim=0
     layers=[]
-    def __init__(self,imageDimension=(64,64,3),slide=4,qDimension=32,kDimension=32):
+
+    def center():
+        pass
+
+    def __init__(self,imageDimension=(64,64,3),qDimension=32,kDimension=32,nOfPatches=16,stride=4,patchesDim=16,firstBests=8,getFeatures=center):
         super(AgentNetwork,self).__init__()
         self.imageDimension = imageDimension
-        self.slide = slide
+        self.stride = stride
+        self.getFeatures = getFeatures
+        self.firstBests = firstBests
+        self.qDimension = qDimension
+        self.kDimension = kDimension
+        self.nOfPatches = (self.imageDimension[0]/self.stride)**2
+        self.patchesDim = self.stride**2
         self.controller=Controller(self.featuresDimension(),15)
-        self.attention=SelfAttention(self.inputDimension,self.qDimension, self.kDimension)
+        self.attention=SelfAttention(self.patchesDim*self.imageDimension[2],self.qDimension, self.kDimension)
         self.layers.append(self.attention)
         self.layers.append(self.controller)
         self.f=AgentNetwork.center
@@ -48,15 +59,16 @@ class AgentNetwork(torch.nn.Module):
         pass
 
     def getOutput(self,input):
-        patches=self.getPatches(input,self.stride)
-        attention=self.attention(patches)
-        bestPatches=self.getBestPatches(attention)
-        actions=self.controller(bestPatches)
+        #n s s c
+        self.patches=self.getPatches(input,self.stride)
+        attention=self.attention(self.patches.view(self.nOfPatches,-1))
+        bestPatches,indices=self.getBestPatches(attention)
+        features=self.getFeatures(bestPatches,indices)
+        actions=self.controller(bestPatches,self.firstBests)
         output=self.selectAction(actions)
         return output
 
-    def center():
-        pass
+    
     def getPatches(self,obs,stride):
         patches = image.extract_patches_2d(obs, (stride,stride))
         return patches
@@ -66,7 +78,13 @@ class AgentNetwork(torch.nn.Module):
         return torch.argmax(actions)
 
     def getBestPatches(self,attention):
-        pass
+        #attention nof patches**2
+        patchesAttention=attention.sum(dim=0)
+        sorted,indices=patchesAttention.sort(descending=True)
+        bests=sorted[:self.bests]
+        indices=indices[:self.bests]
+        return bests,indices
+
     def getParameters(self):
         pass
     
