48025fe2a704aadc44fe15968f329db1eaa14dbb
Andrea <bbooss97@gmail.com>
2022-08-08 19:54:01 +0200

    added wandb

--- parallelEvolution.py
+++ parallelEvolution.py
@@ -4,16 +4,26 @@ from parallelAgent import AgentNetwork
 from parallelGymEnvironment import Gymenv1player
 import tensorflow as tf
 import torch
+import time
 # from pympler.tracker import SummaryTracker
+import wandb
+# import wandb
+# run = wandb.init()
+# artifact = run.use_artifact('bbooss97/attentionAgent/model:v6', type='model')
+# artifact_dir = artifact.download()
 
+# wandb.join()
+num=5
+startagain=False
+agent=AgentNetwork(color=False,qDimension=3,kDimension=3,firstBests=10,num=num)
+name="starpilot lstm"
+run=wandb.init(project='attentionAgent', entity='bbooss97',name=name)
+run.watch(agent)
+time.sleep(5)
 def regularization(params,coeff):
     p=np.array(params)
     regularization= coeff*float(((p**2).sum())**0.5)
     return regularization
-num=5
-startagain=False
-agent=AgentNetwork(color=False,qDimension=3,kDimension=3,firstBests=10,num=num)
-
 
 if startagain:  
     agent=agent.loadModel("./parameters.pt")
@@ -24,17 +34,21 @@ else:
     parameters=[float(0)for i in range(parameters)]
 
 
-variance=10
+variance=20
 es=cma.CMAEvolutionStrategy(parameters,variance)
 j=0
 whenToCopy=100
 
 agent.cuda()
+
 game="starpilot"
 globalBest=-1000
+start=0
+
 # tracker = SummaryTracker()
 with tf.device('/GPU:0'):
     while True:
+        start+=1
         generatedParameters=es.ask()
         fitness=[]
         for i in generatedParameters:
@@ -48,10 +62,14 @@ with tf.device('/GPU:0'):
         print(currentBest)
         if currentBest>globalBest:
             print("saving current best")
+            artifact = wandb.Artifact('model', type='model',)
+            artifact.add_file('./current.pt')
+            run.log_artifact(artifact)
+            # run.finish()
             globalBest=currentBest
             agent.saveModel(str(globalBest))
         es.disp()
-        
+        run.log({"iteration":start,"globalBest":globalBest})
         # tracker.print_diff()
 
 print(es.result.xbest)
